8215f74e7e6797f752c59bb70c0f0b63c5988312
Lisa Han <ellesis@gmail.com>
2022-11-15 12:10:18 -0800

    add tailwind CSS sample

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
+  <h1 class="text-3xl font-bold underline bg-yellow-400">Hello world!</h1>
+
   <header>
-    <h1 class="text-3xl font-bold underline bg-yellow-400">Hello world!</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
